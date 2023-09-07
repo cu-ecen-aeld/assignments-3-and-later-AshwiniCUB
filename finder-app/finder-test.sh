9a61e83dd779f87222aee51837f65d30119ab8fc
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-07 02:36:13 -0600

    Assignment1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,7 +54,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	bash -x ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
  
 OUTPUTSTRING=$(bash -x ./finder.sh "$WRITEDIR" "$WRITESTR")
