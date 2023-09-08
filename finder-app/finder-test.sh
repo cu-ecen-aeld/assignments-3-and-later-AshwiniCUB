a0a3227f2ee42a298e8d6d1662e16ec09c6115ba
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-07 22:50:56 -0600

    Assignment1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,10 +54,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	bash -x ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
  
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(bash -x ./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
