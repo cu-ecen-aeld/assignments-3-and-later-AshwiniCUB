9f60e9337a5cff961627b78a374a754d1b6fad6e
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-08 00:05:47 -0600

    Assignment1

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,6 +4,11 @@
 #Date  : 08/05/2023
 #Reference: referred chatGPT to understand how to write the script and to debug & solve errors
 
+# a full path to a file including the filename on the filesystem
+writefile="$1"
+# text string that need to write into the file
+writestr="$2"
+
 # Check if the script got exactly 2 arguments or not
 if [ $# -ne 2 ]
 then
@@ -11,16 +16,10 @@ then
     exit 1
 fi
 
-# a full path to a file including the filename on the filesystem
-writefile="$1"
-# text string that need to write into the file
-writestr="$2"
-
 # Create the directory path if it doesn't exist
 writefile_dir=$(dirname "$writefile")
-if ! [ -d "$writefile_dir" ]; then
-    mkdir -p "$writefile_dir" && touch "$writefile" 
-    echo $writestr >> $writefile   
-fi
+
+mkdir -p "$writefile_dir" && touch "$writefile" 
+echo $writestr >> $writefile   
 
 exit 0
