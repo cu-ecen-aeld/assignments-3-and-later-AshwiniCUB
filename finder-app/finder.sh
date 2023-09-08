529a6272dde6823a2ad454f1270b301f51b47d21
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-07 22:46:21 -0600

    Assignment1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -4,11 +4,6 @@
 #Date  : 08/05/2023
 #Reference: referred chatGPT to understand how to write the script
 
-# path to a directory on the filesystem
-filesdir="$1"   
-# text string that need to search in files
-searchstr="$2"
-
 # Check if the script got exactly 2 arguments or not
 if [ "$#" -eq 2 ]; then
     echo "Correct arguments are passed"
@@ -17,6 +12,11 @@ else
     exit 1
 fi
 
+# path to a directory on the filesystem
+filesdir="$1"   
+# text string that need to search in files
+searchstr="$2"
+
 # Check if the specified directory exists or not
 if [ ! -d "$filesdir" ]; then
     echo "ERROR: '$filesdir' is not a valid directory"
@@ -24,9 +24,7 @@ if [ ! -d "$filesdir" ]; then
 fi
 
 # Count the number of matching lines in the directory and its subdirectories
-matching_lines_count=$(grep -Rw ${filesdir} -e "${searchstr}" | wc -l)
+matching_lines_count=$(grep -rnw "$filesdir" -e "$searchstr" | wc -l)
 
 # Print the final results
 echo "The number of files are $(find "$filesdir" -type f | wc -l) and the number of matching lines are $matching_lines_count"
-
-exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Description: writer.sh shell script
+#Author: Ashwini Patil
+#Date  : 08/05/2023
+#Reference: referred chatGPT to understand how to write the script and to debug & solve errors
+
+# a full path to a file including the filename on the filesystem
+writefile="$1"
+# text string that need to write into the file
+writestr="$2"
+
+# Check if the script got exactly 2 arguments or not
+if [ "$#" -eq 2 ]; then
+    echo "Correct arguments are passed"
+else
+    echo "ERROR: Invalid number of arguments"
+    exit 1
+fi
+
+# Create the directory path if it doesn't exist
+writefile_dir=$(dirname "$writefile")
+if [ -d "$writefile_dir" ] then
+    cd "$writefile_dir"
+else
+    mkdir -p "$writefile_dir"
+    cd "$writefile_dir"
+fi
+
+
+# Check if the write operation was successful
+if [ -f $writefile ] then
+# Attempt to write the content to the file
+    echo "$writestr" > "$writefile"
+    echo "Write operation is successful"
+else
+    echo "Error: Could not write to '$writefile'"
+    exit 1
+fi
+
