a43e0d0086785c26515a8cb78f2c815e5d6a9e8a
AshwiniCUB <ashwini.patil@colorado.edu>
2023-09-08 01:14:50 -0600

    Assignment1

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,15 +4,22 @@
 #Date  : 08/05/2023
 #Reference: referred chatGPT to understand how to write the script and to debug & solve errors
 
+# Check if the script got exactly 2 arguments or not
+if [ $# -ne 2 ]
+then
+    echo "Invalid number of arguments"
+    exit 1
+fi
+
 # a full path to a file including the filename on the filesystem
 writefile=$1
 # text string that need to write into the file
 writestr=$2
 
-# Check if the script got exactly 2 arguments or not
-if [ $# -ne 2 ]
+# Check if the write operation was successful
+if [ -z "$writefile" ] || [ -z "$writestr" ]
 then
-    echo "Invalid number of arguments"
+    echo "Write file path or string is not specified"
     exit 1
 fi
 
